e38949d8d02a15a5efa40841ac2227193683642d
Dilip Mallya Kasargod <dmallya93@gmail.com>
2026-01-07 15:46:28 -0800

    Revert "Milestone [1] Complete Email Slicer Java Migration"

--- src/main/java/com/example/emailslicer/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Email Slicer application for parsing email addresses.
- * <p>
- * This package contains the Email Slicer CLI tool that takes an email address
- * as input and returns the username and domain as output.
- * </p>
- */
-package com.example.emailslicer;
