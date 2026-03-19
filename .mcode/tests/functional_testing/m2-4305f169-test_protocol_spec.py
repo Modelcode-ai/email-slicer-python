066167014367961c6c71ba823d3c4a84ee2e9fb6
mcode-bot@modelcode.ai <mcode-bot@modelcode.ai>
2026-03-19 21:11:41 +0000

    Origin spec validation (milestone 2)

--- .mcode/tests/functional_testing/m2-4305f169-test_protocol_spec.py
+++ .mcode/tests/functional_testing/m2-4305f169-test_protocol_spec.py
@@ -9,7 +9,7 @@ This script supports two modes:
 1. SRC Validation: Tests commands and captures outputs (no expected_stdout/stderr)
 2. DST Contract Validation: Tests commands and validates outputs match expected
 
-Generated at: 2026-03-19T21:08:51.945369+00:00
+Generated at: 2026-03-19T21:11:39.696755+00:00
 Project: emailslicer-dkasargod
 Milestone: 2
 """
